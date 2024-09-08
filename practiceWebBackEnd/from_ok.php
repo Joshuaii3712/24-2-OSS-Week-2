4a078868c128fe94e27357881e7cb1e0e25cede9
Joshuaii3712 <joshuaii3712@gmail.com>
2024-09-08 23:34:40 +0900

    Upload week2 assignment

---
+++ practiceWebBackEnd/from_ok.php
@@ -0,0 +1,21 @@
+<?php
+    $userFName = $_GET['userFirstName'];
+    $userLName = $GET['userLastName'];
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    이름: <?= $userFName?><br>
+    성: <?= $userLName?><br>
+</body>
+</html>
